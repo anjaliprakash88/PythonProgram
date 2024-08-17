02e7d7d6e318e8e5d7ee225a9ce33d37e2652c61
anjali p <110221249+anjaliprakash88@users.noreply.github.com>
2024-08-17 17:20:30 +0530

    Add files via upload

--- FindFirst&LastPositionOfElaementInSortedArray.py
+++ FindFirst&LastPositionOfElaementInSortedArray.py
@@ -1,18 +1,3 @@
-items = int(input("enter the number of items that your want to input in array: "))
-target = int(input("enter the targeted value: "))
-x = []
-for i in range(items):
-    x.append(int(input("enter the items:")))
-x.sort()
-print(x)
-for j in range(len(x)):
-    if x[j]  == target:
-        print(j)
-    else:
-        print("-1 -1")
-
-# -----------------------------------------------------------------------------------------------------#
-
 
 arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
 n = len(arr)
@@ -29,7 +14,6 @@ for i in range(n):
     last = i
 
 if first != -1:
-    print("First Occurrence =", first,
-          "\nLast Occurrence =", last)
+    print("First Occurrence =", first, "\nLast Occurrence =", last)
 else:
     print("Not Found")
