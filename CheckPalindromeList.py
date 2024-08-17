afba1636bbb68b89f9063710ed2786c4f1d87080
anjali p <110221249+anjaliprakash88@users.noreply.github.com>
2024-08-17 17:15:36 +0530

    Add files via upload

---
+++ CheckPalindromeList.py
@@ -0,0 +1,5 @@
+example_list = ["level", "deified", "civic", "deified", "level"]
+if example_list == example_list[::-1]:
+    print("strings is a palindrome")
+else:
+    print("not a strings is a palindrome")
